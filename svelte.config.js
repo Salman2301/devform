342b7453330cd7dc39f21512114b97f62bfa2156
Salman <salmanhammed77@gmail.com>
2024-06-08 19:01:12 +0530

    fix base path for prod

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -23,7 +21,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: dev ? '' : '/devform'
+			base: process.env.NODE_ENV === 'production' ?  '/devform' : ""
     }
 	}
 };
